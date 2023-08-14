54d0fa577e35341e2efb35eb794f2a9addeb013b
Daniel Pires <dfpires@gmail.com>
2023-08-14 09:48:53 -0300

    Add files via upload

--- topicos2-20232/App.js
+++ topicos2-20232/App.js
@@ -1,8 +1,10 @@
 
 import HelloWorld from './components/hello-world/HelloWorld'
+import HelloWorld2 from './components/hello-world/HelloWorld2'
+import Light from './components/light/Light'
 
 export default function App(){
 
-  return <HelloWorld/> 
+  return <Light/> 
   
 }
---
+++ topicos2-20232/components/light/Light.js
@@ -0,0 +1,8 @@
+import { SafeAreaView, Text } from 'react-native';
+export default function Light() {
+  return (
+    <SafeAreaView>
+      <Text> App da Lâmpada mágica </Text>
+    </SafeAreaView>
+  );
+}
