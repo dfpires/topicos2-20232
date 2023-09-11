c98d2de9fc26ae17a426f78de4d909df8078873b
Daniel Pires <dfpires@gmail.com>
2023-09-11 09:55:32 -0300

    Add files via upload

---
+++ reactnative-expo-examples/react-native-expo-examples/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import HelloWorld1 from "./examples/1-hello-world/1.HelloWorld1";
+import HelloWorld2 from "./examples/1-hello-world/2.HelloWorld2";
+import TheLight from "./examples/2-the-light/TheLight";
+import MomoLogin from "./examples/3-login-page/1.MomoLogin";
+import FacebookLogin from "./examples/3-login-page/2.FacebookLogin";
+import RegisterForm from "./examples/4-register-form/RegisterForm";
+import InstagramFeed from "./examples/5-instagram-feed/InstagramFeed";
+import RockPaperScissors from "./examples/6-rock-paper-scissors/RockPaperScissors";
+//import StopWatch from "./examples/7-stopwatch/StopWatch";
+import BMICalculator from "./examples/8-bmi-calculator/BMICalculator";
+import WorldwideNews from "./examples/9-news/WorldwideNews";
+
+export default function App() {
+//  return <HelloWorld1 />;
+//  return  <HelloWorld2 />;
+//   return <TheLight />;
+//   return <MomoLogin />;
+//   return <FacebookLogin />;
+//   return <RegisterForm />;
+   return <InstagramFeed />;
+ //  return <RockPaperScissors />;
+ //  return <StopWatch />;
+ //  return <BMICalculator />;
+ //  return <WorldwideNews />;
+}
